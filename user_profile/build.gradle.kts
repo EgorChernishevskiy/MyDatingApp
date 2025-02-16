7777011e54cd2c18e2d8490478b71efbe8c6319a
Tiris <100187552+EgorChernishevskiy@users.noreply.github.com>
2025-02-16 13:06:35 +0400

    Initial commit

---
+++ user_profile/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.4.2"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "org.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+
+	// Spring
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+	// PostgreSQL
+	implementation("org.postgresql:postgresql:42.7.2")
+
+	// Lombock
+	compileOnly ("org.projectlombok:lombok:1.18.30")
+	testCompileOnly ("org.projectlombok:lombok:1.18.30")
+	annotationProcessor ("org.projectlombok:lombok:1.18.30")
+	testAnnotationProcessor ("org.projectlombok:lombok:1.18.30")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
